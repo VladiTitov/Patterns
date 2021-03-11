75c699e32acefd647d29ba9fd61ebef9094f7b4d
Vladislav Titov <65026443+VladiTitov@users.noreply.github.com>
2021-03-11 22:50:59 +0300

    added new pattern

---
+++ Patterns/CompositePattern/Components/Component.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CompositeTemplate.Components
+{
+    abstract class Component
+    {
+        protected string name;
+
+        public Component(string name) => this.name = name;
+
+        public virtual void Add(Component component) { }
+
+        public virtual void Remove(Component component) { }
+
+        public virtual void Print() => Console.WriteLine(name);
+    }
+}
---
+++ Patterns/CompositePattern/Components/File.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompositeTemplate.Components
+{
+    class File : Component
+    {
+        public File(string name) : base(name) { }
+    }
+}
